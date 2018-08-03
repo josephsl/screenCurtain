4aee1e48665d3dac509788ed86be2bc62e6462a6
Joseph Lee <joseph.lee22590@gmail.com>
2018-08-03 12:01:37 -0700

    Add Screen curtain tools menu item 9checkable item) and a command to toggle this from everywhere.
    Requested by several users: add ability to toggle this on or off while running NVDA. This is provided by a check item in Tools menu and an unassigned gesture.

--- addon/globalPlugins/screenCurtain/__init__.py
+++ addon/globalPlugins/screenCurtain/__init__.py
@@ -18,6 +18,9 @@ except:
 	raise RuntimeError("Magnification attribute missing")
 from ctypes import byref
 import winVersion
+import gui
+import wx
+from globalCommands import SCRCAT_TOOLS
 
 TRANSFORM_BLACK = winMagnification.MAGCOLOREFFECT()
 TRANSFORM_BLACK.transform[4][4] = 1.0
@@ -30,13 +33,48 @@ TRANSFORM_DEFAULT.transform[4][4] = 1.0
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
+	_screenCurtainActive = False
+	
 	def __init__(self):
 		super(GlobalPlugin, self).__init__()
 		if (winVersion.winVersion.major, winVersion.winVersion.minor) < (6, 2):
 			raise RuntimeError("This add-on is only supported on Windows 8 and above")
-		winMagnification.Initialize()
-		winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_BLACK))
+		self.toolsMenu = gui.mainFrame.sysTrayIcon.toolsMenu
+		# Translators: The label for the menu item to toggle screen curtain.
+		self.toggleScreenCurtain = self.toolsMenu.AppendCheckItem(wx.ID_ANY, _("Screen curtain"))
+		gui.mainFrame.sysTrayIcon.Bind(wx.EVT_MENU, self.onToggleScreenCurtain, self.toggleScreenCurtain)
+		self.onToggleScreenCurtain(None)
 
 	def terminate(self):
+		super(GlobalPlugin, self).terminate()
 		winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_BLACK))
 		winMagnification.Uninitialize()
+		self._screenCurtainActive = False
+		try:
+			if wx.version().startswith("4"):
+				self.toolsMenu.Remove(self.toggleScreenCurtain)
+			else:
+				self.toolsMenu.RemoveItem(self.toggleScreenCurtain)
+		except: #(RuntimeError, AttributeError, wx.PyDeadObjectError):
+			pass
+
+	# The following functions came from NVDA Core's speech viewer toggle mechanics.
+
+	def onToggleScreenCurtain(self, evt):
+		import tones
+		if not self._screenCurtainActive:
+			winMagnification.Initialize()
+			winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_BLACK))
+			self._screenCurtainActive = True
+			tones.beep(1024, 100)
+		else:
+			winMagnification.SetFullscreenColorEffect(byref(TRANSFORM_BLACK))
+			winMagnification.Uninitialize()
+			self._screenCurtainActive = False
+			tones.beep(512, 100)
+		self.toggleScreenCurtain.Check(self._screenCurtainActive)
+
+	def script_toggleScreenCurtain(self, gesture):
+		self.onToggleScreenCurtain(None)
+	script_toggleScreenCurtain.__doc__ = _("Toggles screen curtain on or off")
+	script_toggleScreenCurtain.category = SCRCAT_TOOLS
